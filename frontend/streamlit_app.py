647c1535a5b3c8f45cccc2c6528ecaa9a5bc74f9
moraes7 <nicolasmoraes.dev@gmail.com>
2026-06-08 20:31:54 -0300

    refactor: pequena refatoração dentro do streamlit_app.py

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -57,9 +57,8 @@ if st.session_state["tela"] == "upload":
     )
 
     if uploaded_file is not None:
-        st.image(uploaded_file, width=100) 
 
-    if uploaded_file is not None:
+        st.image(uploaded_file, width=100)
 
         if uploaded_file.type not in ALLOWED_IMAGE_TYPES:
             st.error("Tipo de arquivo inválido. Envie uma imagem JPG, PNG ou WEBP.")
